1e95d26c7217aa4c73bea06d5657e920e9688957
Julian Gautier <juliangautier@juliangautier.com>
2018-05-15 09:54:40 -0700

    still works without hypercore

--- index.js
+++ index.js
@@ -1,63 +1,32 @@
 #!/usr/bin/env node
 const swarm = require('discovery-swarm');
-const hypercore = require('hypercore');
-const ram = require('random-access-memory');
 const defaults = require('dat-swarm-defaults');
 const readLine = require('readline');
 const userName = require('os').userInfo().username;
+const PassThrough = require('stream').PassThrough;
+const moment = require('moment');
+const chalk = require('chalk');
 const crypto = require('crypto');
-const bufferAlloc = require('buffer-alloc-unsafe');
-const sodium = require('sodium-universal');
 const channelName = process.argv[2];
-const peerFeeds = {}
-const channelFeeds = {};
 const channelHash = crypto.createHash('sha256').update(channelName, 'utf8').digest().toString('hex');
-const discoverySwarm = swarm(defaults({}));
+const myChannelFeed = new PassThrough({ objectMode: true });
+const discoverySwarm = swarm(defaults());
 discoverySwarm.listen();
 discoverySwarm.join(channelHash);
-discoverySwarm.on('connection', (stream, info) => {
-    if (channelFeeds[info.id.toString('hex')]) {
-        return;
-    }
-    const swarmId = channelHash + info.id.toString('hex');
-    const publicKey = derivePublicKey(swarmId);
-    const channelFeed = hypercore((filename) => {
-        return ram();
-    }, publicKey);
+discoverySwarm.on('connection', (stream) => {
+    readLine
+    .createInterface({input: stream, crlfDelay: Infinity})
+    .on('line', line => {
+        const msg = JSON.parse(line)
+        const ts = chalk.green((moment(msg.ts)).format('ddd, hh:mm:ssA'))
+        const from = chalk.yellow(userName)
+        const body = chalk.white(msg.body)
 
-    channelFeeds[info.id.toString('hex')] = channelFeed;
-
-    channelFeed.ready(() => {
-        channelSwarm = swarm(defaults({
-            stream: () => {
-                return channelFeed.replicate({ live: true } );
-            }
-        }));
-        channelSwarm.listen();
-        channelSwarm.join(swarmId);
-        const stream = channelFeed.createReadStream({ live: true } );
-        readLine
-        .createInterface({input: stream, crlfDelay: Infinity})
-        .on('line', line => {
-          console.info(`\r${line}`)
-          user.prompt(true)
-        })
-    });
+        console.info(`\r[${ts}] ${from}: ${body}`)
+        user.prompt(true)
+    })
+    myChannelFeed.pipe(stream);
 });
-const myChannelFeed = hypercore((filename) => {
-    return ram();
-}, { seed: channelHash + discoverySwarm.id.toString('hex') });
-myChannelFeed.ready(() => {
-    const myChannelSwarm = swarm(defaults({
-        stream: () => {
-            return myChannelFeed.replicate({ live: true });
-        }
-    }))
-    myChannelSwarm.listen();
-    myChannelSwarm.join(channelHash + discoverySwarm.id.toString('hex'))
-});
-const myWriteStream = myChannelFeed.createWriteStream();
-//jjjjjjmyWriteStream.write('hello world');
 const user = readLine.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -65,22 +34,8 @@ const user = readLine.createInterface({
 });
 user.on('line', line => {
     if (line.trim().length > 0) {
-        myWriteStream.write(`${userName}> ${line}\r\n`);
+        myChannelFeed.write(JSON.stringify({userName: userName, body: line, ts: moment().valueOf()}) + '\n');
     }
     user.prompt(true);
 });
 user.prompt(true);
-
-derivePublicKey = function (seed) {
-    var publicKey = bufferAlloc(sodium.crypto_sign_PUBLICKEYBYTES)
-    var secretKey = bufferAlloc(sodium.crypto_sign_SECRETKEYBYTES)
-
-    if (seed) {
-      // sha256 the seed to make sure the seed is the right number of bytes
-      seed = crypto.createHash('sha256').update(seed, 'utf8').digest()
-      sodium.crypto_sign_seed_keypair(publicKey, secretKey, seed)
-    }
-
-    return publicKey;
-
-}
